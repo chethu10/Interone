3ae1f11f657af2793e289e4ed20070e464009e93
chethu10 <chethan@interone.io>
2022-09-21 19:05:29 +0530

    Creating Blog comp

--- src/Utils/Blog/BlogProps.js
+++ src/Utils/Blog/BlogProps.js
@@ -1,20 +1,23 @@
 import PropTypes from "prop-types";
 
-
-function BlogProps(props){
-    return(
+function BlogProps(props) {
+  return (
+    <div className="blog">
         <div>
-            <img src={props.image} alt=""/>
-            {props.content}
-            {props.main}
-        </div>
-
-    )
+      <div>
+        <img className="BlogImages" src={props.image} alt="" />
+      </div>
+      <div>
+        <div>{props.content}</div>
+        <div>{props.main}</div>
+      </div>
+      </div>
+    </div>
+  );
 }
-BlogProps.prototype={
-image:PropTypes.string,
-content:PropTypes.string,
-main:PropTypes.string
-
-}
+BlogProps.prototype = {
+  image: PropTypes.string,
+  content: PropTypes.string,
+  main: PropTypes.string,
+};
 export default BlogProps;
